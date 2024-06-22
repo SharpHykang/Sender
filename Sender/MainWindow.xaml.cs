da1f52a68950503c05b839cc156fefcbaed5842e
HYKANG <1912238255@qq.com>
2024-06-22 21:42:57 +0800

    添加项目文件。

---
+++ Sender/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Sender.RabbitMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sender
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private RabbitMqService _rabbitMqService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeRabbitMq();
+        }
+
+        //创建RabbitMqService实例
+        private void InitializeRabbitMq()
+        {
+            _rabbitMqService = new RabbitMqService("localhost", "myQueue");
+        }
+
+        /*点击发送状态*/
+        private void sendStatus_click(object sender, RoutedEventArgs e)
+        {
+            bool isChecked = false;
+            string selectedTag=null;
+            foreach (UIElement element in radioGroup.Children)
+            {
+                if (element is RadioButton radioButton && radioButton.IsChecked==true)
+                {
+                    isChecked = true;
+                    selectedTag = radioButton.Tag.ToString();
+                    break;
+                }
+            }
+            if (isChecked)
+            {
+                //发送消息到 RabbitMQ
+                _rabbitMqService.SendMessage(new KeyValuePair<string, object>("status", selectedTag));
+            }
+            else
+            {
+                MessageBox.Show("请选择状态！");
+            }
+        }
+
+        /*点击发送图片*/
+        private void sendPicture_click(object sender, RoutedEventArgs e)
+        {
+            string fileNameStr = fileName.Text.Trim();
+            if (fileNameStr != "")
+            {
+                //发送消息到 RabbitMQ
+                _rabbitMqService.SendMessage(new KeyValuePair<string, object>("fileName", fileNameStr));
+            }
+            else
+            {
+                MessageBox.Show("请输入图片文件名！");
+            }
+        }
+
+
+        //在窗口关闭时释放RabbitMQ连接资源。
+        protected override void OnClosed(EventArgs e)
+        {
+            _rabbitMqService.Dispose();
+            base.OnClosed(e);
+        }
+    }
+}
---
+++ Sender/RabbitMQ/RabbitMqService.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Sender.RabbitMQ
+{
+    //封装RabbitMQ的连接、发送和接收功能
+    public class RabbitMqService
+    {
+        private readonly string _hostName;
+        private readonly string _queueName;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMqService(string hostName, string queueName)
+        {
+            _hostName = hostName;
+            _queueName = queueName;
+            InitializeConnection();
+        }
+
+        //初始化连接:设置连接到RabbitMQ服务器并声明队列。
+        private void InitializeConnection()
+        {
+            var factory = new ConnectionFactory() { HostName = _hostName };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        //发送消息:将消息编码为字节数组，并发布到队列。
+        public void SendMessage(KeyValuePair<string, object> message)
+        {
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+            _channel.BasicPublish(exchange: "", routingKey: _queueName, basicProperties: null, body: body);
+        }
+
+        //接收消息:使用 EventingBasicConsumer 接收消息，并将消息传递给回调函数。
+        public void StartReceivingMessages(Action<KeyValuePair<string, object>> onMessageReceived)
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var json = Encoding.UTF8.GetString(body);
+                var message = JsonConvert.DeserializeObject<KeyValuePair<string, object>>(json);
+                onMessageReceived(message);
+            };
+            _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+        }
+
+        //关闭连接
+        public void Dispose()
+        {
+            //关闭RabbitMQ通道，释放与队列的连接资源。
+            _channel?.Close();
+            //关闭RabbitMQ连接，释放与服务器的连接资源。
+            _connection?.Close();
+        }
+    }
+}
